e44d10a3ad6e1ad544e41c60126cfa40aee9d0e7
Aluno <profissional.guilhermevitor@gmail.com>
2024-09-24 09:56:29 -0300

    Update

--- test_app/src/components/List.jsx
+++ test_app/src/components/List.jsx
@@ -3,18 +3,33 @@ import { useState } from 'react'
 
 
 const List = () => {
-    const array = [1,2,3,4,5]
+    const array = [{
+      nome:"Guilherme",numero:1
+    },{
+      nome:"Joao",numero:3
+    },{
+      nome:"Barbara",numero:3
+    },{
+      nome:"Robson",numero:3
+    },{
+      nome:"Eduardo",numero:3
+    }]
     const [num,setNum]= useState("")
+    const filteredDataNome = array.filter((item)=>item.nome.toLowerCase().includes(num.toLowerCase())
+    
+  )
     function handleClick(element){
         setNum(element.target.value)
     }
-    const filterData=array.filter((item)=>item)
+    
+
   return (
     
     <div>
         <input type="text" onChange={handleClick} name="" id="" />
-        <p>{array.filter((element)=>String(element).includes("1"))}</p>
-        <p>s</p>
+        {array.map((el)=><p>{el.nome}</p>)}
+        <div>{filteredDataNome.map((item)=>(<h1>{item.nome}</h1>))}</div>
+        
     </div>
   )
 }
